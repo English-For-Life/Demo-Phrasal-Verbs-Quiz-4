4d2c4bdfe47c60bb5b0585adc29ed2426a414577
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-29 23:26:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Do Steve and Jess get ___ ?",
+        options:["up", "out", "away", "along"],
+        answer: 3
+    },
+    {
+        q:"Can you tell me where I should get ___ ?",
+        options:["off", "through", "over", "down to"],
+        answer: 0
+    },
+    {
+        q:"We hope to get ___ for a few days at Easter.",
+        options:["out", "away", "off", "down"],
+        answer: 1
+    },
+    {
+        q:"How did Ann get ___ here? She hasn't got the key, has she?",
+        options:["on", "up", "in", "out"],
+        answer: 2
+    },
+    {
+        q:"It took me two weeks to get ___ the terrible flu.",
+        options:["up", "by", "through", "over"],
+        answer: 3
+    }
+]
